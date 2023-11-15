2656b72d0eaa33fff83c8b4591b289bc32d888b5
MosNes <7hekarl@gmail.com>
2023-11-15 14:28:00 -0500

    updated package.json to use module instead of commonjs

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const admin = require("firebase-admin");
+#!/usr/bin/env node
 
-const serviceAccount = require("./service_accounts/zoho-portal-service-account-credentials.json");
+// const admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// const serviceAccount = require("./service_accounts/zoho-portal-service-account-credentials.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+import * as admin from 'firebase-admin';
+import * as csv from 'csvtojson';
+import * as fs from 'fs-extra';
+import * as args from "commander";
+import * as credentials from "./service_accounts/zoho-portal-service-account-credentials.json";
+
+args
+    .version('0.0.1')
+    .option('-s, --src <path>', 'Source file path')
+    .option('-c, --collection <path>', 'Collection path in database')
+    .option('-i, --id [id]', 'Field to use for Document ID')
+    .parse(process.argv);
 
