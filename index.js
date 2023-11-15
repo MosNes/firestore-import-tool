7d5870e50c37e849691e6215cdc022778e2e9de9
MosNes <7hekarl@gmail.com>
2023-11-15 16:02:42 -0500

    debugged broken promise

--- index.js
+++ index.js
@@ -8,17 +8,17 @@
 //   credential: admin.credential.cert(serviceAccount)
 // });
 
-import * as admin from 'firebase-admin';
-import * as csv from 'csvtojson';
+import admin from 'firebase-admin';
 import * as fs from 'fs-extra';
 
 //create instance of Command
 import { Command } from "commander";
-const args = new Command();
 
 // allow use of require for JSON files
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
 const require = createRequire(import.meta.url); // construct the require method
+
+const csv = require('csvtojson');
 
 // import service account credentials from JSON file
 const credentials = require("./service_accounts/zoho-portal-service-account-credentials.json");
@@ -31,13 +31,19 @@ admin.initializeApp({
 // initialize firestore database
 const db = admin.firestore();
 
+const program = new Command();
+
 // configure available command line arguments
-args
+program
     .version('0.0.1')
     .option('-s, --src <path>', 'Source file path')
     .option('-c, --collection <path>', 'Collection path in database')
     .option('-i, --id [id]', 'Field to use for Document ID')
     .parse(process.argv);
+
+const args = program.opts();
+console.log('Input args:', args);
+
 
 // main migration function
 async function migrate() {
@@ -62,6 +68,7 @@ async function migrate() {
         }
 
         if (file.includes('.csv')) {
+            console.log('Detected CSV File');
             data = await readCSV(file);
         }
 
@@ -90,27 +97,38 @@ async function migrate() {
 }
 
 // function to read CSV files
-function readCSV(path) {
-    // wrap the csvtojson module function in a promise, since it is based on callbacks
-    return new Promise((resolve, reject) => {
-    
-        let lineCount = 0;
+async function readCSV(path) {
 
-        csv()
-            .fromFile(path)
-            // on each line, convert to javascript object and increment line count
-            .on('json', data => {
-                // fired on every row read
-                lineCount++;
-            })
-            // on reaching end of file, resolve the promise
-            .on('end parsed', data => {
-                console.info(`CSV read complete. ${lineCount} lines parsed.`);
-                resolve(data);
-            })
-            // on error, reject the promise
-            .on('error', err => reject(err));
-    });
+    console.log('Reading CSV File')
+    
+    try {
+        const jsonArray = await csv().fromFile(path);
+        return jsonArray;
+    } catch(err) {
+        console.log('Error reading CSV file:', err);
+    }
+    
+    // wrap the csvtojson module function in a promise, since it is based on callbacks
+//     return new Promise((resolve, reject) => {
+
+        
+//         let lineCount = 0;
+
+//         csv()
+//             .fromFile(path)
+//             // on each line, convert to javascript object and increment line count
+//             .on('json', data => {
+//                 // fired on every row read
+//                 lineCount++;
+//             })
+//             // on reaching end of file, resolve the promise
+//             .on('end parsed', data => {
+//                 console.info(`CSV read complete. ${lineCount} lines parsed.`);
+//                 resolve(data);
+//             })
+//             // on error, reject the promise
+//             .on('error', err => reject(err));
+//     });
 }
 
 // Run function
