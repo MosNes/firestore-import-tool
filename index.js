094322096d0ccc9c776899788c0911dd90ba1d41
MosNes <7hekarl@gmail.com>
2023-11-15 14:36:06 -0500

    debugged import/require bug and command bug, added comments

--- index.js
+++ index.js
@@ -11,9 +11,27 @@
 import * as admin from 'firebase-admin';
 import * as csv from 'csvtojson';
 import * as fs from 'fs-extra';
-import * as args from "commander";
-import * as credentials from "./service_accounts/zoho-portal-service-account-credentials.json";
 
+//create instance of Command
+import { Command } from "commander";
+const args = new Command();
+
+// allow use of require for JSON files
+import { createRequire } from "module"; // Bring in the ability to create the 'require' method
+const require = createRequire(import.meta.url); // construct the require method
+
+// import service account credentials from JSON file
+const credentials = require("./service_accounts/zoho-portal-service-account-credentials.json");
+
+// initialize firebase app with service account credentials
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+});
+
+// initialize firestore database
+const db = admin.firestore();
+
+// configure available command line arguments
 args
     .version('0.0.1')
     .option('-s, --src <path>', 'Source file path')
