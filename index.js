f1285fef16a70cd8cbd3f3c3d558cfcbc3eb8e24
MosNes <7hekarl@gmail.com>
2023-11-15 16:06:46 -0500

    removed extra comments

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 #!/usr/bin/env node
 
-// const admin = require("firebase-admin");
-
-// const serviceAccount = require("./service_accounts/zoho-portal-service-account-credentials.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-
 import admin from 'firebase-admin';
 import * as fs from 'fs-extra';
 
